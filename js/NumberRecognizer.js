eca7e8c8ef4b1357b9c7d702a6892101315c791e
kiranpariyar <kirannpariyar@gmail.com>
2015-12-23 20:56:56 +0545

    created NumberRecognizer class

---
+++ js/NumberRecognizer.js
@@ -0,0 +1,54 @@
+function NumberRecognizer() {
+	
+
+	this.recognizeNumber = function(ctx,imageWidth,imageHeight) {
+		var recognizedNumber;
+
+	    var imageData = ctx.getImageData(0,0,imageWidth,imageHeight);
+        console.log("initial imageData :",imageData);
+
+        var blackAndWhiteConverter = new BlackAndWhiteConverter();
+        var blackAndWhiteImageData = blackAndWhiteConverter.convertIntoBlackAndWhite(imageData);
+        console.log("blackAndWhiteImageData :",blackAndWhiteImageData);
+        ctx.putImageData(blackAndWhiteImageData,0,0);
+
+   
+        var segmentor = new ImageSegmentation();
+        // // var segmentedImageDataArray = segmentor.segmentImage(ctx,imageData);
+        var segmentedImageDataArray = segmentor.segmentImage(ctx,blackAndWhiteImageData);
+        console.log("segmented image Data array :",segmentedImageDataArray);
+
+        
+        var imgDownScaler = new ImageDownScaler();
+        var scaleDownDataArray = [];
+        var scaleDownImage;
+
+        for (var i = 0; i < segmentedImageDataArray.length; i++) {
+            scaleDownImage = imgDownScaler.getScaleDownImageData(i,ctx,segmentedImageDataArray[i]);
+            var blackAndWhiteScaleDown = blackAndWhiteConverter.convertIntoBlackAndWhite(scaleDownImage);
+            // scaleDownDataArray.push(scaleDownImage);
+            scaleDownDataArray.push(blackAndWhiteScaleDown);
+        };
+
+        console.log("scaleDownDataArray :",scaleDownDataArray);
+
+        
+        var dataRepresentor = new DataRepresentor();
+        var binaryDataArray = [];     
+
+        for (var i = 0; i < scaleDownDataArray.length; i++) {
+                binaryDataArray.push(dataRepresentor.representImageData(scaleDownDataArray[i]));
+        };
+
+        console.log("binaryDataArray :",binaryDataArray);
+        var imageDataMatcher = new ImageDataMatcher();
+        recognizedNumber = imageDataMatcher.matchWithStoredImage(binaryDataArray);
+        for (var i = 0; i < binaryDataArray.length; i++) {
+            imageDataMatcher.getPercentageMatched(i,binaryDataArray[i]);    
+        }
+        
+        console.log("recognizedNumber is :",recognizedNumber);
+		
+        return recognizedNumber;	
+	}
+}
