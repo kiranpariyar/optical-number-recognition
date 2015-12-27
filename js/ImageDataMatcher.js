76189d911abbde404111a076d2c56c47bbb3979f
kiranpariyar <kirannpariyar@gmail.com>
2015-12-27 13:38:56 +0545

    removed euclidean distance matching concept

--- js/ImageDataMatcher.js
+++ js/ImageDataMatcher.js
@@ -1,70 +1,24 @@
 function ImageDataMatcher() {
-	
-	this.matchWithStoredImage = function(sourceDataArray) {
-
-		var euclideanDistance = new EuclideanDistance();
-		var matchedNumber = [];
-
-		for (var i = 0; i < sourceDataArray.length; i++) {
-            
-			var distances = euclideanDistance.getEuclideanDistance(sourceDataArray[i]);
-			var keysSorted = Object.keys(distances).sort(function(a,b){return distances[a]-distances[b]});
-    		var number = keysSorted[0];
-    		// console.log('number is :', number ,distances[number]);
-    		switch(keysSorted[0]){
-
-    			case 'zero':matchedNumber.push(0);
-    						break;
-    			case 'one': matchedNumber.push(1);
-    						break;
-    			case 'two': matchedNumber.push(2);
-    						break;
-				case 'three':matchedNumber.push(3);
-    						break;
-    			case 'four': matchedNumber.push(4);
-    						break;
-    			case 'five': matchedNumber.push(5);
-    						break;
-    			case 'six': matchedNumber.push(6);
-    						break;
-    			case 'seven': matchedNumber.push(7);
-    						break;
-    			case 'eight': matchedNumber.push(8);
-    						break;
-    			case 'nine': matchedNumber.push(9);
-    						break;			
-    		} 
-		};
-
-
-		return matchedNumber;
-	}
 
     this.getPercentageMatched = function(dataNumber,sourceData) {
         
         var numbers = { 
-            zero : [[-1,1,1,1,-1, 1,-1,-1,-1,1, 1,-1,-1,-1,1, 1,-1,-1,-1,1, 1,-1,-1,-1,1 ,1,-1,-1,-1,1, 1,-1,-1,-1,1, -1,1,1,1,-1]],
-            one : [[-1,-1,1,-1,-1, -1,1,1,-1,-1, 1,-1,1,-1,-1, -1,-1,1,-1,-1, -1,-1,1,-1,-1, -1,-1,1,-1,-1, -1,-1,1,-1,-1, 1,1,1,1,1],
-                    [-1,1,1,1,1, -1,-1,-1,1,1, -1,-1,-1,1,1, -1,-1,-1,1,1, -1,-1,-1,1,1, -1,-1,-1,1,1, -1,-1,-1,1,1, -1,-1,-1,1,1],
-                    ],
-            two : [[1,1,1,1,1, -1,-1,-1,-1,1, -1,-1,-1,-1,1, -1,-1,-1,1,-1, -1,-1,1,-1,-1, -1,1,-1,-1,-1, 1,-1,-1,-1,-1, 1,1,1,1,1],
-                    [-1,1,1,1,-1,  1,-1,-1,1,-1, -1,-1,-1,-1,1, -1,-1,-1,-1,1, -1,-1,-1,1,-1, -1,-1,1,-1,-1, -1,1,-1,-1,-1, 1,1,1,1,1]],
-            three : [[1,1,1,1,-1, 1,-1,-1,-1,1, -1,-1,-1,-1,1, -1,-1,1,1,-1, -1,-1,-1,-1,1, -1,-1,-1,-1,1, 1,-1,-1,-1,1, 1,1,1,1,-1]],
-            four : [[-1,-1,-1,1,-1, -1,-1, 1,1,-1, -1,-1, 1,1,-1, -1,1,-1,1,-1, 1,-1,-1,1,-1, 1,1,1,1,1, -1,-1,-1,1,-1, -1,-1,-1,1,-1]],
-            five : [[1,1,1,1,1, 1,-1,-1,-1,-1, 1,-1,-1,-1,-1, 1,1,1,1,-1, -1,-1,-1,-1,1, -1,-1,-1,-1,1, 1,-1,-1,-1,1, 1,1,1,1,-1]],
-            six : [[-1,1,1,1,1, 1,-1,-1,-1,1, 1,-1,-1,-1,-1, 1,1,1,1,-1, 1,-1,-1,-1,1, 1,-1,-1,-1,1, 1,-1,-1,-1,1, -1,1,1,1,-1]],
-            seven : [[1,1,1,1,1, -1,-1,-1,-1,1, -1,-1,-1,1,-1, -1,-1,-1,1,-1, -1,-1,1,-1,-1, -1,-1,1,-1,-1, -1,1,-1,-1,-1, -1,1,-1,-1,-1]],
-            eight : [[-1,1,1,1,-1, 1,-1,-1,-1,1, 1,-1,-1,-1,1, -1,1,1,1,-1, 1,-1,-1,-1,1, 1,-1,-1,-1,1, 1,-1,-1,-1,1, -1,1,1,1,-1],
-                    [1,1,1,1,1, 1,-1,-1,-1,1, 1,-1,-1,-1,1, 1,1,1,1,1, 1,-1,-1,-1,1, 1,-1,-1,-1,1, 1,-1,-1,-1,1, 1,1,1,1,1]],
-            nine : [[-1,1,1,1,-1, 1,-1,-1,-1,1, 1,-1,-1,-1,1, 1,-1,-1,-1,1, -1,1,1,1,1, -1,-1,-1,-1,1, 1,-1,-1,-1,1, -1,1,1,1,-1]]
+            '0' : [[-1,1,1,1,-1, 1,1,-1,1,1, 1,-1,-1,-1,1, 1,-1,-1,-1,1, 1,-1,-1,-1,1 ,1,-1,-1,-1,1, 1,1,-1,1,1, -1,1,1,1,-1]],
+            '1' : [[-1,1,1,-1,-1, 1,-1,1,-1,-1, -1,-1,1,-1,-1, -1,-1,1,-1,-1, -1,-1,1,-1,-1, -1,-1,1,-1,-1, -1,-1,1,-1,-1, 1,1,1,1,1],
+                   [-1,1,1,1,1, -1,-1,-1,1,1, -1,-1,-1,1,1, -1,-1,-1,1,1, -1,-1,-1,1,1, -1,-1,-1,1,1, -1,-1,-1,1,1, -1,-1,-1,1,1]],
+            '2' : [[1,1,1,1,-1, 1,-1,-1,1,1, -1,-1,-1,-1,1, -1,-1,-1,1,1, -1,-1,-1,1,-1, -1,-1,1,-1,-1, 1,1,-1,-1,-1, 1,1,1,1,1],
+                   [-1,1,1,1,-1,  1,-1,-1,1,-1, -1,-1,-1,-1,1, -1,-1,-1,-1,1, -1,-1,-1,1,-1, -1,-1,1,-1,-1, -1,1,-1,-1,-1, 1,1,1,1,1]],
+            '3' : [[1,1,1,1,-1, -1,-1,-1,-1,1, -1,-1,-1,-1,1, -1,-1,1,1,-1, -1,-1,-1,1,1, -1,-1,-1,-1,1, -1,-1,-1,-1,1, 1,1,1,1,-1]],
+            '4' : [[-1,-1,-1,1,-1, -1,-1, 1,1,-1, -1,1, -1,1,-1, -1,1,-1,1,-1, 1,-1,-1,1,-1, 1,1,1,1,1, -1,-1,-1,1,-1, -1,-1,-1,1,-1]],
+            '5' : [[1,1,1,1,1, 1,-1,-1,-1,-1, 1,-1,-1,-1,-1, 1,1,1,1,-1, -1,-1,-1,1,1, -1,-1,-1,-1,1, 1,-1,-1,-1,1, 1,1,1,1,-1]],
+            '6' : [[-1,1,1,1,1, -1,1,-1,-1,-1, 1,-1,-1,-1,-1, 1,1,1,1,-1, 1,-1,-1,-1,1, 1,-1,-1,-1,1, 1,1,-1,-1,1, -1,1,1,1,-1]],
+            '7' : [[1,1,1,1,1, -1,-1,-1,-1,1, -1,-1,-1,1,-1, -1,-1,-1,1,-1, -1,-1,1,1,-1, -1,-1,1,-1,-1, -1,-1,1,-1,-1, -1,1,-1,-1,-1]],
+            '8' : [[-1,1,1,1,-1, 1,-1,-1,-1,1, 1,-1,-1,-1,1, -1,1,1,1,-1, 1,1,-1,1,1, 1,-1,-1,-1,1, 1,-1,-1,-1,1, -1,1,1,1,-1]],
+            '9' : [[-1,1,1,1,-1, 1,-1,-1,1,1, 1,-1,-1,-1,1, 1,-1,-1,-1,1, -1,1,1,1,1, -1,-1,-1,-1,1, -1,-1,-1,1,-1, 1,1,1,1,-1]]
         };
 
         var totalPixel = 40;
         var percentageMatchedObject = {};
-
-        //matching source data with 0 to 9
-        console.log('*********');
-        console.log('matching ',dataNumber+1,'th number with 0 to 9:');
 
         for(var number in numbers){
 
@@ -84,20 +38,17 @@ function ImageDataMatcher() {
                         unmatchedPixel++;
                    }
                 }
-                // console.log('matched pixel with :',number ,matchedPixel);
 
                 percentageMatched = matchedPixel/totalPixel * 100;
                 percentageMatchedArray.push(percentageMatched);
-                // console.log('matchedPixel with :',number,matchedPixel);
-                // console.log('unmatchedPixel with :',number,unmatchedPixel);
-                // console.log('matched percentage with:',number,percentageMatched);
             }
 
-            percentageMatchedArray.sort(function(a,b){return b-a});
-            // console.log(percentageMatchedArray);    
+            percentageMatchedArray.sort(function(a,b){return b-a});  
             percentageMatchedObject[number] = percentageMatchedArray[0];
         }
         
         console.log('matched percentage :',percentageMatchedObject);
+        return percentageMatchedObject;
     }
+
 }
