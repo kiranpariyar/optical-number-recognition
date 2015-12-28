6ab57fc99f0edf14b24b70021170fbfd8e0d1a75
kiranpariyar <kirannpariyar@gmail.com>
2015-12-28 16:59:32 +0545

    added average value black and white converter technique

--- js/BlackAndWhiteConverter.js
+++ js/BlackAndWhiteConverter.js
@@ -1,36 +1,15 @@
 function BlackAndWhiteConverter() {
 
 	this.convertIntoBlackAndWhite = function(imageData){
-
 		var data = imageData.data;
-		var red;
-		var green;
-		var blue;
-		var alpha;
+		var thresholdValue = 100;
+		var avgValue;
 
-		for (var i = 0; i < data.length; i += 4) {
-			red = data[i];
-			green = data[i+1];
-			blue = data[i+2];
-			alpha = data[i+3];
-
-			if( (red > 200 && green > 200 && blue > 200) || (red == 0 && green == 0 && blue == 0 && alpha == 0)){
-				data[i] = 255;
-				data[i+1] = 255;
-				data[i+2] = 255;
-				data[i+3] = 255; 
-			}else{
-				data[i] = 0;
-				data[i+1] = 0;
-				data[i+2] = 0;
-				data[i+3] = 255;
-			}
-
+		for(var i = 0; i < data.length; i += 4){
+			avgValue = (0.2126*data[i] + 0.7152*data[i + 1] + 0.0722*data[i + 2] >= thresholdValue) ? 255 : 0;
+			data[i] = data[i + 1] = data[i + 2] = avgValue; 
+			data[i + 3] = 255; 
 		}
-
-		// console.log('black and white imageData :',imageData);
-		// ctx.clearRect(0,0,500,650);
-		// ctx.putImageData(imageData,0,0);
 
 		return imageData;
 	}
